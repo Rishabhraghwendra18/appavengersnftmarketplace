ce7ac57d36a485e010c43a4d89210cfcdb768e5e
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-09-30 17:29:36 +0530

    added mint section

--- src/containers/Mint/index.js
+++ src/containers/Mint/index.js
@@ -1,19 +1,31 @@
 import { useState } from "react";
 import { useMintNFT, useContract, Web3Button,useAddress } from "@thirdweb-dev/react";
+import { NFTStorage, File } from 'nft.storage'
 import "./index.css";
 
 const NFT_COLLECTION_ADDRESS = process.env.REACT_APP_NFT_COLLECTION;
+const NFT_STORAGE_KEY = process.env.REACT_APP_IPFS;
+
+const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
 
 function Mint() {
   const address = useAddress();
   const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadingImage, setuploadingImage] = useState(false);
   const [nftName, setNftName] = useState('');
   const [nftDescription, setNftDescription] = useState('');
   const { contract } = useContract(NFT_COLLECTION_ADDRESS);
   const { mutateAsync: mintNft, isLoading, error } = useMintNFT(contract);
-  const handleFileChange = (event) => {
+  const handleFileChange = async (event) => {
     const file = event.target.files[0];
-    setSelectedFile(file);
+    setuploadingImage(true);
+    const result = await nftstorage.store({
+      image:file,
+      name:nftName,
+      description:nftDescription,
+  });
+    setSelectedFile(result.data.image.href);
+    setuploadingImage(false);
   };
   return (
     <div>
@@ -28,11 +40,13 @@ function Mint() {
       type="text"
       placeholder="name"
       onChange={(e)=>{setNftName(e.target.value)}}
+      disabled={uploadingImage}
       />
       <input
       type="text"
       placeholder="description"
       onChange={(e)=>{setNftDescription(e.target.value)}}
+      disabled={uploadingImage}
       />
         <Web3Button
           contractAddress={NFT_COLLECTION_ADDRESS}
@@ -41,7 +55,7 @@ function Mint() {
               metadata: {
                 name: nftName,
                 description: nftDescription,
-                image: "ipfs://example.com/my-nft.png", // Accepts any URL or File type
+                image: selectedFile, // Accepts any URL or File type
               },
               to: address, // Use useAddress hook to get current wallet address
             })
